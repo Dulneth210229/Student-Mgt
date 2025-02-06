0c55a2294d1d3762b4376b12b985dc6e940dbd4d
Dulneth210229 <dulneth.sa@gmail.com>
2025-02-06 09:53:12 +0530

    Implemented the fetch all students server file (php file)

---
+++ fetch_students.php
@@ -0,0 +1,28 @@
+<?php
+    include('config.php');
+
+    //set the content-type header to json
+    header('Content-Type : application/json');
+
+    //SQL statement to get data from the database
+    $sql = "SELECT * FROM student";
+
+    $result = mysqli_query($conn, $sql);
+
+    if(!$result){
+        die("Error fetching students" . mysqli_error($conn));
+    }
+
+    //Fetch all rows as an associative array
+    $rows = mysqli_fetch_assoc($result);
+
+    //Encoded result as json and output it
+    echo json_encode($rows);
+
+    //Free the result set
+    mysqli_free_result($result);
+
+    //Close the connection
+    mysqli_close($conn);
+
+?>
